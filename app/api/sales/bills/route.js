6ccfb0713039e683a3fd06f3ba3ea28935bc1051
Harshit4646 <harshitsharma4646@gmail.com>
2026-01-02 18:08:21 +0530

    Update route.js

--- app/api/sales/bills/route.js
+++ app/api/sales/bills/route.js
@@ -2,11 +2,13 @@ import { getDB } from "@/db/connection";
 
 export async function GET() {
   const db = getDB();
+
   const [rows] = await db.query(`
-    SELECT *
+    SELECT id, sale_date, total_amount, payment_type
     FROM sales
     WHERE sale_date >= DATE_SUB(CURDATE(), INTERVAL 7 DAY)
     ORDER BY sale_date DESC
   `);
+
   return Response.json(rows);
 }
