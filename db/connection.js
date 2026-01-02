4c26b42ca6db7f7b260ae75c74e5252aa6c62eca
Harshit4646 <harshitsharma4646@gmail.com>
2026-01-02 17:13:25 +0530

    Update connection.js

--- db/connection.js
+++ db/connection.js
@@ -3,9 +3,10 @@ import mysql from "mysql2/promise";
 const pool = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: true } : false,
+  waitForConnections: true,
+  connectionLimit: 5
 });
 
 export default pool;
