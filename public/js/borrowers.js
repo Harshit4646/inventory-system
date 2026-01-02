40c04326169d38ee1ff53d6c455313a6f262aa05
Harshit4646 <harshitsharma4646@gmail.com>
2026-01-02 17:24:53 +0530

    Update borrowers.js

--- public/js/borrowers.js
+++ public/js/borrowers.js
@@ -1,31 +1,12 @@
-import { getBorrowers, payBorrower } from "./api.js";
-
-const borrowerTable = document.getElementById("borrowerTable");
-
-async function loadBorrowers() {
-  const borrowers = await getBorrowers();
-  borrowerTable.innerHTML = "";
-  borrowers.forEach(b => {
-    const tr = document.createElement("tr");
-    tr.innerHTML = `
-      <td>${b.name}</td>
-      <td>${b.balance}</td>
-      <td>
-        <button onclick="pay('${b.name}')">Pay</button>
-      </td>
-    `;
-    borrowerTable.appendChild(tr);
+fetch("/api/borrowers")
+  .then(r => r.json())
+  .then(d => {
+    document.getElementById("borrowers").innerHTML =
+      `<tr><th>Name</th><th>Due</th></tr>` +
+      d.map(b => `
+        <tr>
+          <td>${b.name}</td>
+          <td>${b.total_due}</td>
+        </tr>
+      `).join("");
   });
-}
-
-window.pay = async (name) => {
-  const cash = parseInt(prompt("Cash paid:", "0")) || 0;
-  const online = parseInt(prompt("Online paid:", "0")) || 0;
-  const discount = parseInt(prompt("Discount:", "0")) || 0;
-
-  await payBorrower({ name, paidCash: cash, paidOnline: online, paidDiscount: discount });
-  loadBorrowers();
-};
-
-// Initial load
-loadBorrowers();
