d7dfca4b92c34725bf472c05d591d5db24d15ac7
Harshit4646 <harshitsharma4646@gmail.com>
2026-01-03 12:33:39 +0530

    Update route.js

--- app/api/stock/route.js
+++ app/api/stock/route.js
@@ -1,5 +1,5 @@
 import mysql from "mysql2/promise";
-
+export const dynamic = "force-dynamic";
 /* ---------- DB CONNECTION (Railway MySQL) ---------- */
 let pool;
 
@@ -60,4 +60,5 @@ export async function POST(req) {
     );
   }
 }
+
 
